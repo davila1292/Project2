a7b69645a2037502ae83e0278b3c4451de5a1c32
lenovo <lenovo@LAPTOP-MJ4J3446>
2025-01-29 20:10:03 -0600

    This is a simple code...

--- src/demo1/DemoTest2.java
+++ src/demo1/DemoTest2.java
@@ -4,7 +4,11 @@ public class DemoTest2 {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Java");
+		System.out.println("Java & GitHub");
+		
+		String str = "Let's make it simple";
+		
+		System.out.println(str);
 		
 	}
 
